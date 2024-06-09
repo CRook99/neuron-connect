74ee76a1599c8cb85e6d5e862035b489c122cef0
CJRook43 <cjrook43@gmail.com>
2024-06-09 18:25:54 -0400

    Sidebar anim working with basic button

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,7 @@ import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import HelpSidebar from "./components/HelpSidebar";
 
-import { useState } from "react";
-
 function App() {
-  // const temp: React.CSSProperties = {
-  //   position: "absolute",
-  //   left: "15%",
-  //   top: "50%",
-  //   transform: "translate(0%, -50%)",
-  // };
-
   return (
     <DndProvider backend={HTML5Backend}>
       <Header />
--- src/components/HelpSidebar.tsx
+++ src/components/HelpSidebar.tsx
@@ -1,15 +1,10 @@
 import "./HelpSidebar.css";
 import Slider from "react-slick";
-import { useSpring, animated, config } from "@react-spring/web";
 import { useState } from "react";
+import { motion } from "framer-motion";
 
 const HelpSidebar = () => {
   const [active, setActive] = useState(false);
-
-  const appear = useSpring({
-    right: active ? "0%" : "-25%",
-    position: "absolute",
-  });
 
   const sliderSettings = {
     dots: true,
@@ -25,22 +20,31 @@ const HelpSidebar = () => {
 
   return (
     <>
-      <animated.div className="sidebar" style={appear}>
-        <h1>How to use NeuroConnect</h1>
-        <Slider {...sliderSettings} className="slider">
-          <div>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse convallis lacus libero, sed tristique tellus laoreet
-              quis. Nam sed odio nunc. Morbi imperdiet, ante in elementum
-              pretium, ex est maximus diam, sed fringilla diam ipsum a augue.
-            </p>
-          </div>
-          <div>
-            <h2>Types of neurons</h2>
-          </div>
-        </Slider>
-      </animated.div>
+      <motion.div
+        className="container"
+        layout
+        initial={{ x: "100%" }}
+        animate={{ x: active ? "0%" : "100%" }}
+        transition={{ type: "spring", stiffness: 300, damping: 30 }}
+      >
+        <div className="sidebar">
+          <h1>How to use NeuroConnect</h1>
+          <Slider {...sliderSettings} className="slider">
+            <div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Suspendisse convallis lacus libero, sed tristique tellus laoreet
+                quis. Nam sed odio nunc. Morbi imperdiet, ante in elementum
+                pretium, ex est maximus diam, sed fringilla diam ipsum a augue.
+              </p>
+            </div>
+            <div>
+              <h2>Types of neurons</h2>
+            </div>
+          </Slider>
+        </div>
+      </motion.div>
+
       <button onClick={handleOnClick}>Click me</button>
     </>
   );
