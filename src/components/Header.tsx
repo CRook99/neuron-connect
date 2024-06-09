a1ccc0934ea7329533177093be0e857572a7c432
CRook99 <cjrook43@gmail.com>
2024-06-09 19:55:05 -0400

    Rename

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ function Header(props: HeaderProps) {
   return (
     <>
       <header className="header">
-        <p className="logo">NeuroConnect</p>
+        <p className="logo">NeuroCircuit</p>
         <nav className="navbar">
           {props.children}
           <div>About</div>
