a2a708ff43eff432f9344a60342a55b2f99d2ba4
CRook99 <cjrook43@gmail.com>
2024-06-22 15:22:55 -0400

    Neuron hover scale

--- src/components/Neuron.tsx
+++ src/components/Neuron.tsx
@@ -1,5 +1,6 @@
 import "./Neuron.css";
 import { Neurons } from "./Neurons";
+import { motion } from "framer-motion";
 
 interface NeuronProps {
   imgPath: string;
@@ -9,9 +10,9 @@ interface NeuronProps {
 export const Neuron = (props: NeuronProps) => {
   return (
     <>
-      <div className="neuron">
+      <motion.div className="neuron" whileHover={{ scale: 1.1 }}>
         <img src={props.imgPath} />
-      </div>
+      </motion.div>
     </>
   );
 };
